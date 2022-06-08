ddf432bddfa8bbb2d60c68cea00aebd405e71354
foxnorth228 <khitrii03@gmail.com>
2022-06-08 19:45:14 +0300

    feat: implement compress/decompress commands functions

--- fileManager/commandsManager/commands/compress.js
+++ fileManager/commandsManager/commands/compress.js
@@ -1,5 +1,30 @@
-import { checkAccess } from "../checkFileAccess.js";
+import { checkAccess, makePathAbsolute } from "../checkFileAccess.js";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import { createBrotliCompress } from "zlib";
 
-export function compress() {
-    
+export async function compress(nonProcessedInput) {
+    if (nonProcessedInput.length > 2) {
+        throw new SyntaxError("Number of arguments is too big")
+    }
+    let isExistNewNameFile = true;
+    const oldName = await checkAccess(nonProcessedInput[0]);
+    let newName;
+    try {
+        newName = await checkAccess(nonProcessedInput[1] + '.br');
+    } catch(err) {
+        isExistNewNameFile = false;
+        newName = await makePathAbsolute(nonProcessedInput[1] + '.br');
+    }
+
+    if(isExistNewNameFile){
+        throw new Error(`${newName} file exists now`); 
+    } else {
+        await pipeline(
+            createReadStream(oldName),
+            createBrotliCompress(),
+            createWriteStream(newName),
+        )
+        console.log("File successfully compressed");
+    }
 }
--- fileManager/commandsManager/commands/decompress.js
+++ fileManager/commandsManager/commands/decompress.js
@@ -1,5 +1,30 @@
-import { checkAccess } from "../checkFileAccess.js";
+import { checkAccess, makePathAbsolute } from "../checkFileAccess.js";
+import { createReadStream, createWriteStream } from "fs";
+import { pipeline } from "stream/promises";
+import { createBrotliDecompress } from "zlib";
 
-export function decompress() {
-    
+export async function decompress(nonProcessedInput) {
+    if (nonProcessedInput.length > 2) {
+        throw new SyntaxError("Number of arguments is too big")
+    }
+    let isExistNewNameFile = true;
+    const oldName = await checkAccess(nonProcessedInput[0]);
+    let newName;
+    try {
+        newName = await checkAccess(nonProcessedInput[1]);
+    } catch(err) {
+        isExistNewNameFile = false;
+        newName = await makePathAbsolute(nonProcessedInput[1]);
+    }
+
+    if(isExistNewNameFile){
+        throw new Error(`${newName} file exists now`); 
+    } else {
+        await pipeline(
+            createReadStream(oldName),
+            createBrotliDecompress(),
+            createWriteStream(newName),
+        )
+        console.log("File successfully compressed");
+    }
 }
