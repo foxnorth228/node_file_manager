a63f49e600a03e81d84d381ca8ece886e51ccaf3
foxnorth228 <khitrii03@gmail.com>
2022-06-08 17:18:06 +0300

    feat: implement hash command function

--- fileManager/commandsManager/commands/hash.js
+++ fileManager/commandsManager/commands/hash.js
@@ -1,5 +1,23 @@
 import { checkAccess } from "../checkFileAccess.js";
+import { constants } from "fs";
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
 
-export function hash() {
-    
+export async function hash(nonProcessedInput) {
+    if (nonProcessedInput.length > 1) {
+        throw new SyntaxError("Number of arguments is too big")
+    }
+    const path = await checkAccess(nonProcessedInput[0], constants.R_OK);
+    await print(createReadStream(path));
+}
+
+async function print(readable) {
+    readable.setEncoding('utf8');
+    let data = '';
+    for await (const chunk of readable) {
+      data += chunk;
+    }
+    const hashSum = createHash("sha256");
+    hashSum.update(data);
+    console.log(hashSum.digest("hex"));
 }
